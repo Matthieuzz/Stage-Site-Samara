2de8ef62cd5f911f9ac9cdb153d49737457e4496
Marion592001 <marion.halle2001@gmail.com>
2023-04-19 10:47:54 +0200

    accueil

---
+++ Slider JS/MenuBurger.js
@@ -0,0 +1,22 @@
+// // Menu burger en JavaScript //
+
+// // On crée des constantes pour le bouton et le menu burger //
+// let btnBurger = document.querySelector("#btn-burger");
+// let nav = document.querySelector(".main-nav-burger");
+
+// // Utilisons maintenant addEventListener pour "écouter l'évenement" //
+
+// btnBurger.addEventListener("click", () => {
+//     // Code à effectuer si l'utilisateur clique sur btnBurger
+//     nav.classList.toggle("visible");
+// }
+// );
+
+//  // () => représente le code à effectuer lorsque l'utilisateur clique sur boutonBurger, toggle permet de "basculer" en "visible" en gros ça ouvre le menu //
+
+
+
+
+
+
+
